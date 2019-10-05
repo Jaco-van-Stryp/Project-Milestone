4b1ab4d0de344bd8a87363f8e1899de8ea4a571a
Jaco van Stryp <jacovanstryp@gmail.com>
2019-10-05 19:18:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@ namespace Project_Milestone
 {
     class Program
     {
+        //Daniel Van Stryp Programming 152 - Project Milestone
+        enum menu
+        {
+            add = 1,
+            displayAll,
+            search,
+            modify,
+            delete,
+            sort,
+            normalize,
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Daniiel's Milestone Project!");
+            displayMenu(); //Displaying menu to the user
+        }
 
+        private static void displayMenu()
+        {
+            Console.WriteLine("Please select one of the following");
         }
     }
 }
