41d331963662502004883807b49de4f74ac5ba74
Jaco van Stryp <jacovanstryp@gmail.com>
2019-10-05 19:58:57 +0200

    Added the Add Method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Project_Milestone
 {
@@ -16,9 +17,11 @@ namespace Project_Milestone
             normalize,
         }
 
+        //Fixed Array of 10'000 Entries
+       static String[] expenses = new string[10000];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Daniiel's Milestone Project!");
             displayMenu(); //Displaying menu to the user
         }
 
@@ -32,6 +35,71 @@ namespace Project_Milestone
             Console.WriteLine("5) Delete Item");
             Console.WriteLine("6) Sort Items Alphabetically");
             Console.WriteLine("7) Normalize Descriptions");
+
+            int input = int.Parse(Console.ReadLine());
+
+            menu mainMenu = (menu)input;
+
+            switch (mainMenu)
+            {
+                case menu.add:
+                    addExpense();
+                    break;
+                case menu.displayAll:
+                    break;
+                case menu.search:
+                    break;
+                case menu.modify:
+                    break;
+                case menu.delete:
+                    break;
+                case menu.sort:
+                    break;
+                case menu.normalize:
+                    break;
+                default:
+                    break;
+            }
+            displayMenu();
+
+        }
+
+        public static void addExpense()
+        {
+            int totalLeft = 10000 - expenses.Count(s => s != null);
+            int curTotal = expenses.Count(s => s != null);
+            Console.WriteLine("---------------------------------");
+
+            Console.WriteLine("Let's add a new expense!\nHow many entries do you want to add?");
+            int totalEntries = int.Parse(Console.ReadLine());
+            if (totalLeft - totalEntries <= 0)
+            {
+                Console.WriteLine("You can only store a max of 10'000 Expenses In this program!");
+            }
+            else
+            {
+               
+                for (int i = curTotal; i < curTotal + totalEntries; i++)
+                {
+                    Console.WriteLine("Adding Entry #" + (i + 1)+ " Into The List");
+                    String date = "";
+                    while (date.Length != 8) //validating the length of 8
+                    {
+                        Console.WriteLine("Please enter the purchase date in format YYYYMMDD (8 Charaacters)");
+                        date = Console.ReadLine();
+                    }
+                    Console.WriteLine("Please enter the Description of the Expenditure or Item");
+                    String desc = Console.ReadLine();
+                    Console.WriteLine("What category is this?");
+                    String category = Console.ReadLine();
+                    Console.WriteLine("What is the amount for this Expenditure or Item?");
+                    String amount = Console.ReadLine();
+                    expenses[i] = date + "☺" + desc + "☺" + category + "☺" + amount;
+                }
+
+            }
+            Console.WriteLine("---------------------------------");
+
 
         }
     }
